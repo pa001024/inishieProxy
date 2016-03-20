549c0d414ca3f564940f43bd7cec75aeffb37956
pa001024 <pa001024@qq.com>
2016-03-21 01:00:18 +0900

    add more cli flag

--- main.go
+++ main.go
@@ -17,9 +17,16 @@ var resourceInterceptor *ResourceInterceptor
 func main() {
 	lang := flag.String("lang", "en_US", "langauge code")
 	port := flag.Int("port", 8066, "port")
+	nocdn := flag.Bool("nocdn", false, "use CDN or not")
+	isDebug := flag.Bool("debug", false, "is debug?")
 	flag.Parse()
 	// env
-	util.DEBUG.SetEnable(true)
+	if *isDebug {
+		util.DEBUG.SetEnable(true)
+	}
+	if *nocdn {
+		ENABLE_CDN = false
+	}
 	// translator
 	resourceInterceptor = NewResourceInterceptor("translate", *lang)
 	// router
